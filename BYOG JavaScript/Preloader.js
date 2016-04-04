8bb718f66279a465afdcb5b4a8177c19faeb7830
kevwang27 <wang.kevin99@gmail.com>
2016-04-04 09:34:39 -0400

    Create Preloader.js

---
+++ BYOG JavaScript/Preloader.js
@@ -0,0 +1,22 @@
+topDownCarGame.Preloader = function (game) {
+    this.titleText = null;
+    this.ready = false;
+}
+
+topDownCarGame.Preloader.prototype = {
+
+    preload: function () {
+        this.titleText = this.add.image(this.world.centerX, this.world.centerY, 'titleimage');
+		    this.titleText.anchor.setTo(0.5, 0.5);
+		    this.load.image('titlescreen',  TITLE SCREEN PIC);
+		    this.load.bitmapfont(ENTER FONT);
+		    this.load.image('road', ROAD PIC);
+        this.load.image('van', CANDY_VAN PIC);
+        this.load.image('audi', AUDI PIC);
+        this.load.image('explosion', EXPLOSION PIC);
+    }
+    
+    create: function () {
+  		  this.state.start('StartMenu');
+	  }
+}

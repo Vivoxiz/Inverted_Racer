8a8bf6d6a5cb74dfd18359bb95679689166a4013
kevwang27 <wang.kevin99@gmail.com>
2016-04-06 08:34:03 -0400

    Update Preloader.js

--- BYOG JavaScript/Preloader.js
+++ BYOG JavaScript/Preloader.js
@@ -6,10 +6,10 @@ topDownCarGame.Preloader.prototype = {
 
     preload: function () {
         this.titleText = this.add.image(this.world.centerX, this.world.centerY, 'titleimage');
-		    this.titleText.anchor.setTo(0.5, 0.5);
-		    this.load.image('titlescreen',  TITLE SCREEN PIC);
-		    this.load.bitmapfont(ENTER FONT);
-		    this.load.image('road', ROAD PIC);
+	this.titleText.anchor.setTo(0.5, 0.5);
+	this.load.image('titlescreen',  TITLE SCREEN PIC);
+	this.load.bitmapfont(ENTER FONT);
+	this.load.image('road', ROAD PIC);
         this.load.image('van', CANDY_VAN PIC);
         this.load.image('audi', AUDI PIC);
         this.load.image('explosion', EXPLOSION PIC);

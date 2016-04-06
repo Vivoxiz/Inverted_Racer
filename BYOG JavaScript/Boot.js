40d14c6c59c25cdd58e8d809dff8d7ce2aa1cc21
kevwang27 <wang.kevin99@gmail.com>
2016-04-06 08:39:04 -0400

    Update Boot.js

--- BYOG JavaScript/Boot.js
+++ BYOG JavaScript/Boot.js
@@ -3,24 +3,23 @@ var topDownCarGame = {};
 topDownCarGame.Boot = function (game) {};
 
 topDownCarGame.Boot.prototype = {
-    preload: function() {
-        this.load.image('titleimage', TITLE GOES HERE);
-    }
+    preload: function () {
+        this.load.image('titleimage', INSERT TITLE TEXT HERE);
+    },
   
-    create: function() {
+    create: function () {
         this.input.maxPointers = 1;
-    		this.stage.disableVisibilityChange = false;
-    		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-    		this.scale.minWidth = 343;
-    		this.scale.minHeight = 480;
-    		this.scale.pageAlignHorizontally = true;
-    		this.scale.pageAlignVertically = true;
-    		this.stage.forcePortrait = true;
-    		this.scale.setScreenSize(true);
-    
-    		this.input.addPointer();
-    		this.stage.backgroundColor = '#171642';
-            
+        this.stage.disableVisibilityChange = false;
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        this.scale.minWidth = 343;
+        this.scale.minHeight = 480;
+		this.scale.pageAlignHorizontally = true;
+		this.scale.pageAlignVertically = true;
+		this.stage.forcePortrait = true;
+		this.scale.setScreenSize(true);
+        this.input.addPointer();
+        this.stage.backgroundColor = '#171642';
+        
         this.state.start('Preloader');
-  	}
-}
+    }
+};

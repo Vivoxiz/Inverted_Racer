865db68044725e5017654d85062918a94c2e56c9
kevwang27 <wang.kevin99@gmail.com>
2016-04-06 09:09:46 -0400

    Update Boot.js

--- BYOG JavaScript/Boot.js
+++ BYOG JavaScript/Boot.js
@@ -4,7 +4,8 @@ topDownCarGame.Boot = function (game) {};
 
 topDownCarGame.Boot.prototype = {
     preload: function () {
-        this.load.image('titleimage', INSERT TITLE TEXT HERE);
+        this.load.image('preloaderBar', 'Assets/loader_bar.png');
+        this.load.image('titleimage', 'Assets/Road_Rash_Logo.png');
     },
   
     create: function () {
@@ -13,10 +14,10 @@ topDownCarGame.Boot.prototype = {
         this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         this.scale.minWidth = 343;
         this.scale.minHeight = 480;
-		this.scale.pageAlignHorizontally = true;
-		this.scale.pageAlignVertically = true;
-		this.stage.forcePortrait = true;
-		this.scale.setScreenSize(true);
+	this.scale.pageAlignHorizontally = true;
+	this.scale.pageAlignVertically = true;
+	this.stage.forcePortrait = true;
+	this.scale.setScreenSize(true);
         this.input.addPointer();
         this.stage.backgroundColor = '#171642';
         
